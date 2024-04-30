9cc5f02afdab39046666e0494822f8cd11682d23
rida-brown <165578416+rida-brown@users.noreply.github.com>
2024-04-30 09:37:15 -0600

    Create vehicle.h

---
+++ vehicle.h
@@ -0,0 +1,38 @@
+#ifndef VEHICLE_H
+# define VEHICLE_H
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+    private: 
+       int year;
+       string make;
+       string model;
+       string color;
+       double price;
+    public:
+       
+       Vehicle(int year, string make, string model, string color, double price) {
+           this-> year = year;
+           this-> make = make;
+           this-> model = model;
+           this-> color = color;
+           this-> price = price;
+       } 
+       void displayInfo() {
+           cout << "Vehicle: " << make << ", " << model <<  ", " << year << ", " << color << ", $" << price << endl;
+       
+       }
+       
+       int getYear();
+       string getMake();
+       string getModel();
+       string getColor();
+       double getPrice();
+       
+       Vehicle();
+       
+};
+
+#endif
