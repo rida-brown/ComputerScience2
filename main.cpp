ef160d94e24a11f420507c0571370190dcf80eb2
rida-brown <165578416+rida-brown@users.noreply.github.com>
2024-05-02 10:38:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,35 +17,9 @@ int main() {
     
  Vehicle car1(2014, "Toyota", "Camry", "green", 14000.00);
  car1.displayInfo();
-    
-    
-    
-    
-//Calling functions    
-    //readCustomer (customers); 
-   // readVehicles (vehicles);
-   // calculateSales (vehicles);
-    
-// Display vehicle info
 
 for(int i=0; i < vehicles.size(); ++i){ 
     vehicles[i].displayInfo();
-    
-    
-    
-/*cout << "Customer Types: \n";
-cout << "1 - Interested: Customers who are inmterested in purchasing vehicles\";
-cout << "2 - Bought: Customers who have already purchased vehicles \";
-
-int customerType;
-cout << "What type of Customer? (1-2): ";
-cin >> customerType;*/
-}
-
-//Display customer info
-for (const Customer& customer: customers) {
-    
-}
 
 
 return 0;
